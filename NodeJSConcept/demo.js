fac0251efa7179373c9fa83dbd5711f46ee2db78
HimanshuP90 <himanshup895@gmail.com>
2018-01-14 03:00:32 +0530

    nodejs concept added

---
+++ NodeJSConcept/demo.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+// var data = require('./data.json');
+
+// console.log(data.name);
+
+// fs.readFile('./data.json', 'utf-8', (err, data) => {
+// 	var data = JSON.parse(data)
+// 	console.log(data.name);
+// });
+
+// fs.readdir('/home/himanshu/Desktop/BCS', (err, data) =>{
+// 	console.log(data)
+// });
+// 
+
+// var data = {
+// 	firstName: 'Himanshu',
+// 	lastName: 'Pandey',
+// 	profession: 'Software Engineer'
+// }
+// fs.writeFile('writeFile_data.json', JSON.stringify(data), (err, data) => {
+// 	console.log('write finished, ', err);
+// });
+
+
---
+++ NodeJSConcept/sync-demo.js
@@ -0,0 +1,6 @@
+fs = require('fs');
+
+data = fs.readdirSync('/home/himanshu/Desktop/BCS');
+console.log("\n data: ", data);
+
+console.log("this comes after \n");
