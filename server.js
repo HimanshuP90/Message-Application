d1c12f6be50ba21a0c08b436aeb76e4463a3c42e
HimanshuP90 <himanshup895@gmail.com>
2018-01-14 12:36:17 +0530

    try and catch added

--- server.js
+++ server.js
@@ -24,26 +24,32 @@ app.get('/messages', (req, res) => {
 });
 
 app.post('/messages', async (req, res) => {
-	var message = new Message(req.body)
 
-	var savedMessage = await message.save()
-	console.log('saved');
-	
-	var censored =  await Message.findOne({message: 'badword'})
+	try {
+		
+		throw 'error'
+		
+		var message = new Message(req.body)
 
-	if (censored)
-		await Message.remove({_id: censored.id})
-	else 
-		io.emit('message', req.body)
+		var savedMessage = await message.save()
 
-	res.sendStatus(200)
-	// })
-	// .catch((err) => {
-	// 	res.sendStatus(500)
-	// 	return console.error(err)
-	// })
+		console.log('saved');
+		
+		var censored =  await Message.findOne({message: 'badword'})
 
-});
+		if (censored)
+			await Message.remove({_id: censored.id})
+		else 
+			io.emit('message', req.body)
+
+		res.sendStatus(200)
+	} catch (err) {
+			res.sendStatus(500)
+			return console.error(err)
+	} finally {
+		console.log('message post called')
+	}
+})
 
 io.on('connection', (socket) => {
 	console.log("user connection");
