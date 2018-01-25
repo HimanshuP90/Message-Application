99ec07d63980aeb8e0bb8e3d48f53d396a0d0e73
HimanshuP90 <himanshup895@gmail.com>
2018-01-25 16:09:44 +0530

    Added Nodejs concept

---
+++ NodeJSConcept/core.js
@@ -0,0 +1,23 @@
+// var stuff = require('./stuff');
+
+// console.log(stuff.counter(['reactjs', 'nodejs', 'redux']));
+
+// console.log(stuff.adder(5,6));
+// console.log(stuff.adder(stuff.pi, 5));
+
+var fs = require('fs');
+
+// fs.readFile('my.txt', 'utf-8', function(err, data){
+// 	fs.writeFile('write.txt', data);
+// });
+
+// fs.unlink('write.txt'); //delete file
+
+// fs.rmdirSync('stuff'); // create dir
+
+//Create directory -> then create Me.txt file
+// fs.mkdir('stuff', function(){
+// 	fs.readFile('my.txt', 'utf-8', function(err,data){
+// 	  fs.writeFile('./stuff/Me.txt', data);
+// 	});
+// });
