233164ada114cff255704761515c90a26865ee25
HimanshuP90 <himanshup895@gmail.com>
2018-01-26 11:30:02 +0530

    read file data and send back to client side

--- NodeJSConcept/Server.js
+++ NodeJSConcept/Server.js
@@ -13,11 +13,14 @@
 // 172.24.86.76:3000
 
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res){
 	console.log('request was made: ' + req.url);
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hey himanshu');
+
+	var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
+	myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
--- NodeJSConcept/stream.js
+++ NodeJSConcept/stream.js
@@ -4,8 +4,10 @@ var fs = require('fs');
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
 var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
-myReadStream.on('data', function(chunk){
-	console.log('new chunk received');
-	myWriteStream.write(chunk);
-});
+// myReadStream.on('data', function(chunk){
+// 	console.log('new chunk received');
+// 	myWriteStream.write(chunk);
+// });
 
+
+myReadStream.pipe(myWriteStream);
