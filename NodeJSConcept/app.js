7e285b59e41b8d07d0b5c1bfc95513acbf66c62a
HimanshuP90 <himanshup895@gmail.com>
2018-01-27 19:54:36 +0530

    Added Template Engines

--- NodeJSConcept/app.js
+++ NodeJSConcept/app.js
@@ -1,13 +1,19 @@
 var express = require('express');
-
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-	res.send('This is the homepage');
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
 	res.send('This is the contact page');
+});
+
+app.get('/profile/:name', function(req, res){
+	var  data = {age: 29, job: 'software engineer'};
+	res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
