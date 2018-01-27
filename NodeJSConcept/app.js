fd8f11d0907bef6fd9e52bdf1f84931a5742be14
HimanshuP90 <himanshup895@gmail.com>
2018-01-28 00:51:23 +0530

    Middleware & Static Files added

--- NodeJSConcept/app.js
+++ NodeJSConcept/app.js
@@ -2,18 +2,23 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.render('index');
 });
 
 app.get('/contact', function(req, res){
-	res.send('This is the contact page');
+	res.render('contact');
 });
 
 app.get('/profile/:name', function(req, res){
-	var  data = {age: 29, job: 'software engineer'};
-	res.render('profile', {person: req.params.name, data: data});
+	var  data = {
+			age: 29, job: 'software engineer',
+			hobbies: ['eating', 'surfing', 'chattingg']
+				};
+	res.render('profile', {person: req.params.name, 
+		data: data});
 });
 
 app.listen(3000);
