9c75e7e8b4b62157b6e82b85dd21a34d754462fa
HimanshuP90 <himanshup895@gmail.com>
2018-01-28 13:18:31 +0530

    Added Handling POST Requests

--- NodeJSConcept/app.js
+++ NodeJSConcept/app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -9,8 +12,14 @@ app.get('/', function(req, res){
 });
 
 app.get('/contact', function(req, res){
-	res.render('contact');
+	res.render('contact', {qs: req.query});
 });
+
+app.post('/contact', urlencodedParser, function(req, res){
+	console.log(req.body);
+	res.render('contact', {qs: req.query});
+});
+
 
 app.get('/profile/:name', function(req, res){
 	var  data = {
